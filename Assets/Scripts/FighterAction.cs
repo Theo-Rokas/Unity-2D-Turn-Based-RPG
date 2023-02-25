953cd6e1d981eaef110f915c13010f14a6c0a189
Theo-Rokas <T.Rokas@hotmail.com>
2023-02-25 23:31:44 +0200

    First Commit

---
+++ Assets/Scripts/FighterAction.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FighterAction : MonoBehaviour
+{
+    private GameObject enemy;
+    private GameObject hero;
+
+    [SerializeField]
+    private GameObject meleePrefab;
+
+    [SerializeField]
+    private GameObject rangePrefab;
+
+    [SerializeField]
+    private Sprite faceIcon;
+
+    private GameObject currentAttack;
+    private GameObject meleeAttack;
+    private GameObject rangeAttack;
+
+    public void SelectAttack(string btn)
+    {
+        GameObject victim = hero;
+        if(tag == "Hero")
+        {
+            victim = enemy;
+        }
+        if(btn.CompareTo("melee") == 0)
+        {
+            Debug.Log("Melee Attack!");
+        }
+        else if(btn.CompareTo("range") == 0)
+        {
+            Debug.Log("Range Attack!");
+        }
+        else
+        {
+            Debug.Log("Run");
+        }
+    }
+}
